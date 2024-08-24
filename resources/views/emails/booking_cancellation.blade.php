336ed46afd6232c5508faafdcb40a3b96efbc52a
agripaa <agrieva.xananda08@gmail.com>
2024-08-24 11:15:15 +0700

    add function delete after send information user ;

--- resources/views/emails/booking_cancellation.blade.php
+++ resources/views/emails/booking_cancellation.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Booking Cancellation</h1>
-    <p>Dear {{ $booking->user->name }},</p>
+    <p>Dear {{ $booking->user->username }},</p>
     <p>Your booking with code {{ $booking->code }} has been canceled. Here is the reason provided:</p>
     <p><strong>{{ $reason }}</strong></p>
     <p>Thank you for using our service.</p>
